19bd19d002449316ea153cda20802e3ec75e4e92
David Moreno <cesardaviddev@gmail.com>
2022-12-02 15:25:27 +0100

    home img play

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,7 +5,8 @@ const Home = () => {
     <>
       <div>
         <h1>Hi, USUARIO(CAMBIAR POR VARIABLE)</h1>
-        
+        <button>Playlist</button>
+        <button>Albums</button>
       </div>
     </>
   );
