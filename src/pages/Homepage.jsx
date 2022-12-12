d113d42c1e03701463b45a82c91330d14055d311
Mimika Ackerman <emily.herreragomez@gmail.com>
2022-12-12 16:00:01 +0100

    jackson

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import AlbumContainer from '../components/AlbumsContainer/AlbumsContainer'
-import ArtistsContainer from '../components/ArtistContainer/ArtistsContainer'
-import GenresContainer from '../components/GenresContainer/GenresContainer'
-import Menu from '../components/Menu/Menu'
-import PlaylistContainer from '../components/PlaylistContainer/PlaylistContainer'
-import TracksContainer from '../components/TracksContainer/TracksContainer'
+import React from "react";
+import AlbumContainer from "../components/AlbumsContainer/AlbumsContainer";
+import ArtistsContainer from "../components/ArtistContainer/ArtistsContainer";
+import GenresContainer from "../components/GenresContainer/GenresContainer";
+import Menu from "../components/Menu/Menu";
+import PlaylistContainer from "../components/PlaylistContainer/PlaylistContainer";
+import TracksContainer from "../components/TracksContainer/TracksContainer";
 
 const Homepage = () => {
   return (
     <>
-    <h1>HomePage</h1>
-<Menu/>
+      <h1>HomePage</h1>
+      <Menu />
 
-    <PlaylistContainer/>
-    <AlbumContainer/>
-    {/* <TracksContainer/> */}
-    <ArtistsContainer/>
+      <PlaylistContainer />
+      <AlbumContainer />
+      {/* <TracksContainer/> */}
+      <ArtistsContainer />
 
-
-<GenresContainer/>
-
+      <GenresContainer />
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
