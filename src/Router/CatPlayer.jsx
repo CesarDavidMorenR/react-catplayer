363a1a8a4cbcc0d7c9694721f6f02bbae910d05f
ThalitaRoque <95291857+ThalitaRoque@users.noreply.github.com>
2022-12-12 17:19:31 +0000

    changebtncolor

--- src/Router/CatPlayer.jsx
+++ src/Router/CatPlayer.jsx
@@ -7,6 +7,7 @@ import Homepage from '../pages/Homepage'
 import SearchPage from '../pages/SearchPage'
 import ListPage from './../pages/ListPage'
 import AccountPage from './../pages/AccountPage'
+import GenresPage from '../pages/GenresPage';
 
 
 const CatPlayer = () => {
@@ -18,6 +19,7 @@ const CatPlayer = () => {
         <Route path='/SearchPage' element={<SearchPage/>}/>
         <Route path="/ListPage" element={<ListPage/>}/>
         <Route path='/AccountPage' element={<AccountPage/>} />
+        <Route path= '/GenresPage' element={<GenresPage/>}/>
 
         
     </Routes>
--- src/components/GenresContainer/GenresContainer.jsx
+++ src/components/GenresContainer/GenresContainer.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { fetchMusic } from "./../../Api/fetchMusic";
+import { fetchMusic } from "./../../API/FetchMusic";
+import { Link } from "react-router-dom";
 import './Genres.css'
 
 
 const GenresContainer = () => {
   const [genres, setGenres] = useState([]);
   console.log(genres);
-  const url = "http://localhost:5000/genres";
+  const url = `http://localhost:8000/genres`;
 
   useEffect(() => {
     const genres = async () => {
@@ -15,6 +16,28 @@ const GenresContainer = () => {
     };
     genres();
   }, [url]);
+
+  // const colors = [
+  //   "#ef5777",
+  //   "#575fcf",
+  //   "#4bcffa",
+  //   "#34e7e4",
+  //   "#0be881",
+  //   "#f53b57",
+  //   "#3c40c6",
+  //   "#0fbcf9",
+  //   "#00d8d6",
+  //   "#05c46b",
+  //   "#ffc048",
+  //   "#ffdd59",
+  //   "#ff5e57",
+  //   "#d2dae2",
+  //   "#485460",
+  //   "#ffa801",
+  //   "#ffd32a",
+  //   "#ff3f34"
+  // ]; 
+
   return (
     <>
       <div>
@@ -24,7 +47,9 @@ const GenresContainer = () => {
             <div className="Btn_container">
         {genres.map((genre) => (
           <div key={genre.id} >
+          <Link to={'/GenresPage'}>
              <button className="Btn-genre">{genre.name}</button>
+             </Link>
             </div>
           
         ))}
