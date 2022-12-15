e5a69aff316e2d6a5d62bb4157a25da0fdfb9886
Mimika Ackerman <emily.herreragomez@gmail.com>
2022-12-15 23:04:28 +0100

    homedesing

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,26 +4,20 @@ import ArtistsContainer from "../../components/ArtistContainer/ArtistsContainer"
 import GenresContainer from "../../components/GenresContainer/GenresContainer";
 import TracksContainer from "../../components/TracksContainer/TracksContainer";
 
-
-
 import Menu from "../../components/Menu/Menu";
 import PlaylistContainer from "../../components/PlaylistContainer/PlaylistContainer";
 import { useAuthContext } from "../../context/LoginContext";
 
 const Homepage = (props) => {
-  const {isAuthenticated} = useAuthContext();
-  const user = JSON.parse(localStorage.getItem("name"))
+  const { isAuthenticated } = useAuthContext();
+  const user = JSON.parse(localStorage.getItem("name"));
   return (
     <>
-
-  {isAuthenticated ? (
-    <div>
-      <span>welcome back,{user}</span>
-    </div>
-  ): null}
-
-
-
+      {isAuthenticated ? (
+        <div>
+          <span>welcome back,{user}</span>
+        </div>
+      ) : null}
 
       <Menu />
       <PlaylistContainer />
--- src/pages/TracksPage/TracksPage.jsx
+++ src/pages/TracksPage/TracksPage.jsx
@@ -4,7 +4,6 @@ import TracksContainer from '../../components/TracksContainer/TracksContainer'
 const TracksPage = () => {
   return (
     <>
-    <h1>TracksPage</h1>
         <TracksContainer/>
     
     </>
