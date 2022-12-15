fb6e24274243586060a945268038f224b0a0c6f8
David Moreno <cesardaviddev@gmail.com>
2022-12-15 18:38:33 +0100

    Player implemented

--- src/components/TracksContainer/TracksContainer.jsx
+++ src/components/TracksContainer/TracksContainer.jsx
@@ -1,13 +1,69 @@
 import React, { useEffect, useState } from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 import useFetchApi from "../../API/useFetchApi";
 import { Link } from "react-router-dom";
+import  './TracksContainer.css'
 const TracksContainer = () => {
   const { tracks } = useFetchApi();
+
+
+    const [currentTrack, setTrackIndex] = useState(0);
+  console.log(currentTrack);
+  // Wasn't working until i put optional chaining operator "?" before url in playlist
+  const playlist = tracks[currentTrack]?.url;
+  const thumbnail = tracks[currentTrack]?.thumbnail;
+  const name = tracks[currentTrack]?.name;
+
+  const artist = tracks[currentTrack]?.artist;
+
+  const genre = tracks[currentTrack]?.genre;
+  const liked = tracks[currentTrack]?.liked;
+
+  console.log(playlist);
+
+  const handleClickNext = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack < tracks.length - 1 ? currentTrack + 1 : 0
+    );
+  };
+
+  const handleClickPrevious = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack < tracks.length - 1 ? currentTrack - 1 : 0
+    );
+  };
+
+  const handleEnd = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack < tracks.length - 1 ? currentTrack + 2 : 0
+    );
+  };
 
   return (
     <>
       <div className="containerBody">
         <section>
+          <div>
+            <img src={thumbnail} alt="" />
+            <p>{name}</p>
+            <p>{artist}</p>
+            <p>{genre}</p>
+            <div>{{ liked } === false ? "❤" : "😨"}</div>
+
+            <AudioPlayer
+              // autoPlay
+              src={playlist}
+              onPlay={(e) => console.log("onPlay")}
+              // other props here
+              showSkipControls={true}
+              showJumpControls={true}
+              onClickNext={handleClickNext}
+              onClickPrevious={handleClickPrevious}
+              onEnded={handleEnd}
+            />
+          </div>
+          <h1 className="title__suggestion">Suggestions You May Like</h1>
           <div className="trackContainer">
             {tracks
               .sort(() => Math.random() - 0.5)
